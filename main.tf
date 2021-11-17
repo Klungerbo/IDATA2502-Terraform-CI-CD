78b249302a0b3edd8fdb99510a146ef369ecfef8
Klungerbo <tomas.kolsen@gmail.com>
2021-11-17 18:56:13 +0100

    Add backend

--- main.tf
+++ main.tf
@@ -7,11 +7,17 @@ terraform {
   }
 }
 
+backend "gcs" {
+  bucket  = "tf-state-prod"
+  prefix  = "terraform/state"
+}
+
 resource "google_storage_bucket" "gcs_bucket" {
   name = "terraform-bucket-idata2502-port-tko"
 }
 
 provider "google" {
+  credentials = "../idata2502-portfolio-1-tko-e83f60880ce2.json.copy"
   project = "idata2502-portfolio-1-tko"
   region  = "europe-west4"
   zone    = "europe-west4-a"
